da49eb9c498fd4f8340db1bca08a5905a94aaa74
yashrawat <yashrawat682@gmail.com>
2025-04-06 20:08:03 +0530

    added environment binding for DATABASE_URL to support Cloudflare Workers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,13 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
+const app = new Hono<{ Bindings: { DATABASE_URL: string } }>()
 
 app.get('/api/v1/blog/:id', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
   return c.text('get blog')
 })
 
