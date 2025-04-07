7b3f2ee396bdb0ba2d9cf62993ba25b990d550ca
yashrawat <yashrawat682@gmail.com>
2025-04-07 15:58:52 +0530

    404 error while sending request

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono<{ Bindings: { DATABASE_URL: string } }>()
+
+
+const app = new Hono<{ Bindings: { DATABASE_URL: string ,JWT_SECRET: string} }>()
 
 app.get('/api/v1/blog/:id', (c) => {
   const prisma = new PrismaClient({
@@ -15,9 +18,34 @@ app.get('/api/v1/blog/bulk', (c) => {
   return c.text('bulk')
 })
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('signup route')
-})
+// sign up route for the user 
+app.post('/api/v1/user/signup', async(c) => {
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+
+try{
+
+  const body = await c.req.json();
+
+  const user =await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      password:body.password
+      }
+    })
+    const token =await sign({id:user.id},c.env.JWT_SECRET);
+
+    return c.json({jwt:token})
+    
+
+  }catch(e){
+    return c.status(500)
+  }
+}
+)
 
 app.post('/api/v1/user/signin', (c) => {
   return c.text('signin route')
